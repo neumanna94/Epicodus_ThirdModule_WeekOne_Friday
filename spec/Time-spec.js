d989a2768351968a37fcf1ffc4c8e9c067c2d92f
Alexander Neumann <alexander.daniel.neumann@gmail.com>
2018-03-16 15:02:08 -0700

    Add test spec for new method CheckLifeExpectancyForDifferentPlanet(inputConversionFactor)

--- spec/Time-spec.js
+++ spec/Time-spec.js
@@ -6,11 +6,11 @@ describe('Time', function() {
   beforeEach(function() {
     thenDate = new Date('December 17, 1995 03:24:00');
     nowDate = new Date();
-    newTime = new Time(thenDate);
+    newTime = new Time(thenDate, 70);
   });
   it('Test Constructor', function() {
     expect(Math.round(newTime.then.getSeconds())).toEqual(Math.round(thenDate.getSeconds()));
-    expect(Math.round(newTime.now.getSeconds()).toEqual(Math.round(nowDate.getSeconds()));
+    expect(Math.round(newTime.now.getSeconds())).toEqual(Math.round(nowDate.getSeconds()));
   });
   it('Check IntegerYearToSeconds for 100 cases.', function() {
     for(var i = 0; i < 100; i ++){
@@ -24,21 +24,22 @@ describe('Time', function() {
   it('Check NowToSeconds()', function() {
     expect(newTime.NowToSeconds()).toEqual(nowDate.getTime()/1000);
   });
-  it('Check SimpleLifeExpectancyGenerator() for 1001 cases.', function() {
-    let age;
-    for(var i = 0; i <= 1000; i ++){
-      age = i;
-      expect(newTime.SimpleLifeExpectancyGenerator(i)).toEqual(i - (newTime.NowToThenDifferenceSeconds())*(1/60)*(1/60)*(1/24)*(1/365.25));
-    }
-  });
   it('Check CheckExceedLifeExpectancy() for 100 cases.', function() {
     //Doesn't exceed life expectancy. Age is 22 years old. i = life expectancy.
     for(var i = 50; i <= 500; i ++){
-      expect(newTime.CheckExceedLifeExpectancy(i)).toEqual(false);
+      newTime = new Time(thenDate, i);
+      expect(newTime.CheckExceedLifeExpectancy()).toEqual(false);
     }
     ////Exceeds. Age is 22 years old. i = life expectancy.
     for(var i = 1; i <= 22; i ++){
-      expect(newTime.CheckExceedLifeExpectancy(i)).toEqual(true);
+      newTime = new Time(thenDate, i);
+      expect(newTime.CheckExceedLifeExpectancy()).toEqual(true);
     }
+  });
+  it('Check CheckLifeExpectancyForDifferentPlanet', function() {
+      let conversionFactors = [.24, .62, 1.88, 11.86];
+      for(var i = 0; i < 4; i ++){
+        expect(newTime.CheckLifeExpectancyForDifferentPlanet(conversionFactors[i])).toEqual(newTime.SimpleLifeExpectancyGenerator()*(1/conversionFactors[i]));
+      }
   });
 });
